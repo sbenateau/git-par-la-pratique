557dce803481e1385d03d612401b82c0de3219d8
sbenateau <sbenateau@mailodie.fr>
2021-03-17 13:52:29 +0100

    first commit

---
+++ presentation/presentation.R
@@ -0,0 +1,38 @@
+---
+  title: Git par la pratique
+author: Alain Danet / https://alaindanet.github.io
+date: \today
+institute: CESCO Lab / MNHN
+output: binb::metropolis
+---
+  
+  ```{r, eval = TRUE, echo = FALSE}
+knitr::opts_chunk$set(
+  echo = TRUE,
+  eval = FALSE,
+  fig.dim = c(10, 7) #width, height
+)
+library(devtools)
+setwd(dirname(getwd()))
+
+```
+
+# Set up a package compendium
+
+## What do you need ?
+
+
+## Resources 
+
+- [R packages book](http://r-pkgs.had.co.nz/) 
+
+## Architecture
+
+- [Publish your code](../papers/Barnes - 2010 - Publish your computer code it is_good_enough.pdf)
+- [Compendium](../papers/Marwick et al. - 2018 - Packaging Data Analytical
+               Work Reproducibly Using .pdf)
+- [Recent example](../papers/Ponisio et al. - 2017 - Opportunistic attachment
+                   assembles plant-pollinato.pdf)
+- [Github repo](https://github.com/lponisio/hedgerow_assembly)
+
+
